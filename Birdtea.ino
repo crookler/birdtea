743a44b57d7a229444e009dfedc20cda8bf225e0
Ethan Crook <ecrook@unc.edu>
2025-04-22 17:14:52 -0400

    Neck rotation functionality

---
+++ Birdtea.ino
@@ -0,0 +1,106 @@
+#include "TemperaturesAndConstants.h"
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <Stepper.h>
+
+OneWire oneWire(ONE_WIRE_BUS); // One wire bus
+DallasTemperature sensors(&oneWire); // Pass pointer to sensors
+Stepper stepper = Stepper(STEPS_PER_REVOLUTION, 2, 4, 3, 5); // Attach stepper to pins 2-5 (IN1 - IN4) - delcared IN1, IN3, IN2, IN4
+
+void setup(void)
+{
+  // Start serial communication and temperature sensor
+  Serial.begin(9600);
+  sensors.begin();
+
+  // Define digital read pins for starting 
+  pinMode(START_BUTTON, INPUT);
+  pinMode(SELECT_BUTTON, INPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+
+  while (!await_select(selected_steep, steeping_presets)) {};
+  delay(200); // Slight debounce delay (so that second select isn't skipped)
+  while (!await_select(selected_cool, cooling_presets)) {};
+
+  // Lower stepper motor (neck control) and start waiting for the selected steep time
+  stepper.setSpeed(5);
+  stepper.step(STEPS_PER_REVOLUTION/4);
+
+  // Wait for the user selected amount of time
+  //waitForSteep(steeping_presets[selected_steep].steep_time);
+
+  // Raise neck and begin waiting for temperature (main loop)
+  stepper.step(-STEPS_PER_REVOLUTION/4);
+  analogWrite(RED, 255); // Reset LED
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 255);
+}
+
+// Use loop mainly for temperature sensing
+void loop(void) { 
+  sensors.requestTemperatures(); 
+  float f_temp = sensors.getTempFByIndex(0);
+  
+  Serial.print("Fahrenheit temperature: ");
+  Serial.println(f_temp);
+
+  if (f_temp < cooling_presets[selected_cool].cool_temp) {
+    // Notify user that tea is at desired temperature
+    while (!signalDrinkTime()) {};
+
+    // Enter end trap Todo: maybe a goto can be used to restart instead of hardware reset button
+    while (true) {};
+  }
+
+  delay(1000);
+}
+
+// Note: Arduino internal clock does not have great accuracy (external timer would be better)
+void waitForSteep(int minutes) {
+  unsigned long start_time = millis();
+  unsigned long wait_duration = minutes * 60000UL; // Convert to ms (unsigned long has lower risk of overflow)
+
+  while (millis() - start_time < wait_duration) {
+    // Flash LED while waiting
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 0);
+    delay(1000);
+
+    analogWrite(RED, 255);
+    analogWrite(GREEN, 255);
+    analogWrite(BLUE, 255);
+    delay(1000);
+
+    // Give updates to serial
+    Serial.print("Estimated Time Remaining: ");
+    Serial.print((wait_duration - (millis() - start_time)));
+    Serial.println(" millis");
+  }
+}
+
+bool signalDrinkTime() {
+  // Flash LED fast until user arrives
+  analogWrite(RED, rand() % 256);
+  analogWrite(GREEN, rand() % 256);
+  analogWrite(BLUE, rand() % 256);
+  delay(100);
+
+  analogWrite(RED, rand() % 256);
+  analogWrite(GREEN, rand() % 256);
+  analogWrite(BLUE, rand() % 256);
+  delay(100);
+
+  // TODO: play speaker sound
+
+  // Check if start button is hit (similar to alarm clock in this case)
+  bool end = digitalRead(START_BUTTON);
+  delay(100); // Debounce
+  return end;
+}
+
+
+
+
---
+++ TemperaturesAndConstants.h
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+
+#define ONE_WIRE_BUS 6
+#define STEPS_PER_REVOLUTION 2048
+//#define STEPS_PER_REVOLUTION 4096
+#define START_BUTTON 7
+#define SELECT_BUTTON 8
+#define RED 11 
+#define GREEN 10
+#define BLUE 9
+
+// Hex codes for color
+typedef struct {
+  int red_code;
+  int green_code;
+  int blue_code;
+} color;
+
+// Define steep and steep time (minutes) and the hex codes for the colors
+typedef struct {
+  int steep_time;
+  color hex_codes;
+} steep;
+
+// Define temperature to notify user (degrees fahrenheit) and colors
+typedef struct {
+  float cool_temp;
+  color hex_codes;
+} temperature;
+
+// Array of tea steeping times and accompnaying index
+const steep steeping_presets[] = {
+  {1, {255, 255, 0}}, // Deep blue
+  {3, {0, 255, 0}}, // Purple
+  {5, {255, 0, 255}}, // Green
+  {10, {0, 255, 255}} // Red
+};
+
+int selected_steep = 0; // Index for above array tracking selected steep time
+
+// Array of structs TODO: fine tune temperatures
+const temperature cooling_presets[] = {
+  {80.0, {0, 175, 255}}, // Bright Orange
+  {70.0, {55, 55, 255}}, // Cool Yellow
+  {60.0, {255, 125, 0}}  // Cool Blue
+};
+
+int selected_cool = 0; // Index for above array tracking selected temperature
+
+// Function for index over one of the above presets (T is a struct and N is the number of members in the strut)
+// Need to declare template right after structs for Arduino compiler for some reason
+template<typename T, size_t N>
+bool await_select(int &index, T (&presets)[N]) { // Read only presets
+  // Advance index and wrap if at end of array
+  if (digitalRead(SELECT_BUTTON)) {
+    index = (index + 1) % N;
+    delay(100); // Debounce
+  }
+
+  Serial.println(index);
+
+  // Write to RGB LED with color presets
+  analogWrite(RED, presets[index].hex_codes.red_code);
+  analogWrite(GREEN, presets[index].hex_codes.green_code);
+  analogWrite(BLUE, presets[index].hex_codes.blue_code);
+
+  // Check if start button is hit
+  bool start = digitalRead(START_BUTTON);
+  delay(100); // Debounce
+  
+  return start;
+}
